dca6d12d10d081b787a1bf51cbbc013291cc1619
wangrangzhi <451155928@qq.com>
2014-04-25 17:55:22 +0800

    new
    new

---
+++ Shapes.java
@@ -0,0 +1,53 @@
+// typeinfo/Shapes3.java
+// TIJ4 Chapter Typeinfo, Exercise 3, page 561
+/* Add Rhomboid to Shapes.java. Create a Rhomboid, upcast it to a Shape,
+* then downcast it back to a Rhomboid. Try downcasting to a Circle and
+* see what happens.'
+*/
+import java.util.*;
+
+abstract class Shape {
+	void draw() { System.out.println(this + ".draw()"); }
+	abstract public String toString();
+}
+
+class Circle extends Shape {
+	public String toString() { return "Circle"; }
+}
+
+class Square extends Shape {
+	public String toString() { return "Square"; }
+}
+
+class Triangle extends Shape {
+	public String toString() { return "Triangle"; }
+}
+
+class Rhomboid extends Shape
+{
+	public String toString(){return "Rh"; }
+}
+
+public class Shapes {
+
+	public static void rotate(Shape s)
+	{
+		if(!(s instanceof Circle))
+			System.out.println(s+"draw");
+	}
+
+	public static void main(String[] args) {
+		List<Shape> shapeList = Arrays.asList(new Circle(), new Triangle());
+
+			for(Shape shape : shapeList)
+		shape.draw();
+
+	for(Shape shape : shapeList)
+		rotate(shape);
+	}
+
+
+
+
+
+}
