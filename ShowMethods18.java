dba71a9cb3f2af397325ebc82db1b2948098d24c
wangrangzhi <451155928@qq.com>
2014-04-27 12:02:28 +0800

    j
    j

--- ShowMethods18.java
+++ ShowMethods18.java
@@ -7,15 +7,16 @@ class ShowMethods18
 	"usage: \n" + 
 	"ShowMethods qualified.class.name\n" +
 	"To show all methods in class or : \n" +
+	"ShowMethods qualified.class.name word\n" +
 	"To search for methods involving 'word' ";
 
-	private static Pattern p = Pattern.comile("\\w+\\.");
+	private static Pattern p = Pattern.compile("\\w+\\.");
 
-	public static void main(String[] main)
+	public static void main(String[] args)
 	{
-		if(args.legnth < 1)
+		if(args.length < 1)
 		{
-			print(usage);
+			System.out.println(usage);
 			System.exit(0);
 		}
 
@@ -25,15 +26,44 @@ class ShowMethods18
 		{
 			Class<?> c = Class.forName(args[0]);
 
-			Method[] methods = c.getmethods();
+			Method[] methods = c.getMethods();
 
 			Constructor[] ctors = c.getConstructors();
 
-			if(args.legnth == 1)
+			if(args.length == 1)
 			{
 				for(Method method : methods)
 				{
 					System.out.println(p.matcher(method.toString()).replaceAll(""));
+				}
+
+				for(Constructor ctor : ctors)
+				{
+					System.out.println(p.matcher(ctor.toString()).replaceAll(""));
+				}
+
+					lines = methods.length + ctors.length;
+
+
+			}else 
+			{
+				for(Method method : methods )
+				{
+					if(method.toString().indexOf(args[1]) != -1)
+					{
+
+					System.out.println(p.matcher(method.toString()).replaceAll(""));
+					lines++;	
+					}
+				}
+
+				for(Constructor ctor : ctors)
+				{
+					if(ctor.toString().indexOf(args[1]) != -1)
+					{
+						System.out.println(p.matcher(ctor.toString()).replaceAll(""));
+						lines++;
+					}
 				}
 			}
 
